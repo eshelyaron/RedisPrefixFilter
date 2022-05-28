5f7d7b61fbad2c5ce59c99f37d0794c5f952e8b0
Eshel Yaron <eshel@areionsec.com>
2022-05-28 20:28:01 +0300

    Add Prefix-Filter submodule and make it work!

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	g++ -shared -fPIC -o module.so module.c
+	g++ -shared --std=c++17 -march=native -fPIC -o module.so module.c Prefix-Filter/Tests/*.cpp Prefix-Filter/*.cpp Prefix-Filter/Prefix-Filter/*.cpp Prefix-Filter/TC-Shortcut/*.cpp
--- module.c
+++ module.c
@@ -1,14 +1,74 @@
 #include "redismodule.h"
 #include <stdlib.h>
+#include "Prefix-Filter/Tests/wrappers.hpp"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+static RedisModuleType * PFType;
+
+int HelloworldRand_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+    RedisModule_ReplyWithLongLong(ctx,rand());
+    FilterAPI<Prefix_Filter<TC_shortcut>>::ConstructFromAddCount(1000000);
+    return REDISMODULE_OK;
+}
+
+
+static int PFReserve_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+    RedisModule_AutoMemory(ctx);
+
+    if (argc < 2) {
+        return RedisModule_WrongArity(ctx);
+    }
+
+    long long capacity;
+    if (RedisModule_StringToLongLong(argv[2], &capacity) != REDISMODULE_OK) {
+        return RedisModule_ReplyWithError(ctx, "ERR bad capacity");
+    } else if (capacity <= 0) {
+        return RedisModule_ReplyWithError(ctx, "ERR (capacity should be larger than 0)");
+    }
+
+    RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ | REDISMODULE_WRITE);
+
+    Prefix_Filter<TC_shortcut> foo = FilterAPI<Prefix_Filter<TC_shortcut>>::ConstructFromAddCount(capacity);
+
+    RedisModule_ModuleTypeSetValue(key, PFType, &foo);
+
+    RedisModule_ReplyWithSimpleString(ctx, "OK");
+
+    return REDISMODULE_OK;
+}
+
+
+static void BFRdbSave(RedisModuleIO *io, void *obj) {}
+
+static void *BFRdbLoad(RedisModuleIO *io, int encver) {
+  return NULL;
+}
+
+static void BFAofRewrite(RedisModuleIO *aof, RedisModuleString *key, void *value) {
+  (void)value;
+}
+
+static void BFFree(void *value) { (void)value; }
+
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     if (RedisModule_Init(ctx,"avromodule",1,REDISMODULE_APIVER_1)
         == REDISMODULE_ERR) return REDISMODULE_ERR;
 
+    if (RedisModule_CreateCommand(ctx,"pf.reserve",
+                                  PFReserve_RedisCommand, "write",
+                                  1, 1, 1) == REDISMODULE_ERR)
+      return REDISMODULE_ERR;
+
+    static RedisModuleTypeMethods typeprocs = {.version = REDISMODULE_TYPE_METHOD_VERSION,
+                                               .rdb_load = BFRdbLoad,
+                                               .rdb_save = BFRdbSave,
+                                               .aof_rewrite = BFAofRewrite,
+                                               .free = BFFree};
+
+    PFType = RedisModule_CreateDataType(ctx, "PFilter--", 0, &typeprocs);
 
     return REDISMODULE_OK;
 }
