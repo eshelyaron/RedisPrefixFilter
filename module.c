4223baa166d71bcae2f46475210ee86f76f2b663
Eshel Yaron <eshel@areionsec.com>
2022-06-05 17:42:52 +0300

    Add pf.add command

--- module.c
+++ module.c
@@ -1,3 +1,9 @@
+#include <bitset>
+#include <functional>
+#include <string>
+#include <vector>
+
+
 #include "redismodule.h"
 #include <stdlib.h>
 #include "Prefix-Filter/Tests/wrappers.hpp"
@@ -14,6 +20,69 @@ int HelloworldRand_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, i
     return REDISMODULE_OK;
 }
 
+static int getValue(RedisModuleKey *key, RedisModuleType *expType, void **sbout) {
+    *sbout = NULL;
+    if (key == NULL) {
+        return -1;
+    }
+    int type = RedisModule_KeyType(key);
+    if (type == REDISMODULE_KEYTYPE_EMPTY) {
+        return -2;
+    } else if (type == REDISMODULE_KEYTYPE_MODULE &&
+               RedisModule_ModuleTypeGetType(key) == expType) {
+        *sbout = RedisModule_ModuleTypeGetValue(key);
+        return 0;
+    } else {
+        return -3;
+    }
+}
+
+
+static int pfGetObject(RedisModuleKey *key, Prefix_Filter<TC_shortcut> **sbout) {
+    printf("pfGetObject\n");
+    int i = getValue(key, PFType, (void **)sbout);
+    printf("%d\n", i);
+    return i;
+}
+
+static int PFAdd_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
+    RedisModule_AutoMemory(ctx);
+    std::hash<std::string> stdhash;
+
+    if (argc < 2) {
+        return RedisModule_WrongArity(ctx);
+    }
+
+    printf("got here\n");
+    RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ | REDISMODULE_WRITE);
+
+    Prefix_Filter<TC_shortcut> *pf = NULL;
+    if (pfGetObject(key, &pf) < 0) {
+      return RedisModule_ReplyWithError(ctx, "error fetching table by key");
+    }
+
+    printf("got there\n");
+    printf("%p\n", pf);
+    if (pf == NULL) {
+      return RedisModule_ReplyWithError(ctx, "internal server error");
+    }
+
+    size_t len = 0;
+    std::string str = RedisModule_StringPtrLen(argv[2], &len);
+
+    unsigned long long h = stdhash(str);
+
+    printf("got here again\n");
+
+    printf("size: %lu\n", FilterAPI<Prefix_Filter<TC_shortcut>>::get_cap(pf));
+
+    FilterAPI<Prefix_Filter<TC_shortcut>>::Add(h, pf);
+
+    RedisModule_ReplyWithSimpleString(ctx, "OK");
+
+    printf("done\n");
+    return REDISMODULE_OK;
+}
 
 static int PFReserve_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     RedisModule_AutoMemory(ctx);
@@ -30,10 +99,18 @@ static int PFReserve_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv,
     }
 
     RedisModuleKey *key = RedisModule_OpenKey(ctx, argv[1], REDISMODULE_READ | REDISMODULE_WRITE);
+    float loads[2] = {.95, .95};
 
-    Prefix_Filter<TC_shortcut> foo = FilterAPI<Prefix_Filter<TC_shortcut>>::ConstructFromAddCount(capacity);
+    Prefix_Filter<TC_shortcut>* foo = new Prefix_Filter<TC_shortcut>(capacity, loads);
 
-    RedisModule_ModuleTypeSetValue(key, PFType, &foo);
+    printf("%p\n", foo);
+    printf("func: %u\n", FilterAPI<Prefix_Filter<TC_shortcut>>::get_functionality(foo));
+    printf("cap: %lu\n", FilterAPI<Prefix_Filter<TC_shortcut>>::get_cap(foo));
+
+    FilterAPI<Prefix_Filter<TC_shortcut>>::Add(9999, foo);
+
+    printf("new cap: %lu\n", FilterAPI<Prefix_Filter<TC_shortcut>>::get_cap(foo));
+    RedisModule_ModuleTypeSetValue(key, PFType, foo);
 
     RedisModule_ReplyWithSimpleString(ctx, "OK");
 
@@ -59,6 +136,11 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
 
     if (RedisModule_CreateCommand(ctx,"pf.reserve",
                                   PFReserve_RedisCommand, "write",
+                                  1, 1, 1) == REDISMODULE_ERR)
+      return REDISMODULE_ERR;
+
+    if (RedisModule_CreateCommand(ctx,"pf.add",
+                                  PFAdd_RedisCommand, "write",
                                   1, 1, 1) == REDISMODULE_ERR)
       return REDISMODULE_ERR;
 
