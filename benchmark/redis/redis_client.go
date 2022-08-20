9166420dd616dc22c2cf1f008686490e382c753c
HadarPX <hadar.tal@perimeterx.com>
2022-08-20 19:25:02 +0300

    wip

--- benchmark/redis/redis_client.go
+++ benchmark/redis/redis_client.go
@@ -14,7 +14,7 @@ type RedisClient struct {
 
 func NewRedisClient() *RedisClient {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6380",
+		Addr:     "localhost:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
