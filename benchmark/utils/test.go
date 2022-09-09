c61e6b701cef3d880deb3f4be0c5b67bf000a704
HadarPX <hadar.tal@perimeterx.com>
2022-09-09 15:51:15 +0300

    printing

--- benchmark/utils/test.go
+++ benchmark/utils/test.go
@@ -2,11 +2,13 @@ package utils
 
 import (
 	"context"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/sync/errgroup"
 	"time"
 )
 
 func RunTest(avgOfIterations, parallelTests int, f func() error) (time.Duration, error) {
+	logrus.Infof("%s\n", time.Now().UTC().String())
 	totalDuration := time.Duration(0)
 	for i := 0; i < avgOfIterations; i++ {
 		g, _ := errgroup.WithContext(context.Background())
@@ -16,6 +18,7 @@ func RunTest(avgOfIterations, parallelTests int, f func() error) (time.Duration,
 		}
 		err := g.Wait()
 		if err != nil {
+			logrus.Error(err)
 			return 0, err
 		}
 		totalDuration += time.Now().Sub(start)
